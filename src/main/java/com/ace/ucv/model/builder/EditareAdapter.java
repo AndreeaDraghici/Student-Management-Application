a33e3bdd1d950d261b07d8396e4c6228a4516d75
AndreeaDraghici <draghici108@gmail.com>
2023-11-02 17:41:28 +0200

    refactor: improved code readability and structure in EditareAdapter class

--- src/main/java/com/ace/ucv/model/builder/EditareAdapter.java
+++ src/main/java/com/ace/ucv/model/builder/EditareAdapter.java
@@ -1,34 +1,21 @@
 package com.ace.ucv.model.builder;
+
 import com.ace.ucv.model.Discipline;
 import com.ace.ucv.model.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.table.DefaultTableModel;
-public class EditareAdapter extends DefaultTableModel{
 
-    private List<Student> studenti = null;
-    private List<Discipline> materii = null;
-    private boolean isStudent = false;
+public class EditareAdapter extends DefaultTableModel {
 
-    public void setMaterii(List<Discipline> materii) {
-        this.materii = materii;
-        isStudent = false;
-    }
-
-    public void setStudenti(List<Student> studenti) {
-        this.studenti = studenti;
-        isStudent = true;
-    }
+    private List<Student> students = new ArrayList<>();
+    private List<Discipline> disciplines = new ArrayList<>();
 
     @Override
     public int getRowCount() {
-        if(studenti == null && materii == null)
-            return 0;
-        if (isStudent) {
-            return studenti.size();
-        }
-        return materii.size();
+        return isStudent() ? getStudentCount() : getDisciplineCount();
     }
 
     @Override
@@ -41,99 +28,126 @@ public class EditareAdapter extends DefaultTableModel{
         if (columnIndex > 4) {
             return "";
         }
-        if (!isStudent) {
-            switch (columnIndex) {
-                case 0:
-                    return "Id";
-                case 1:
-                    return "Denumire";
-                case 2:
-                    return "Profesor";
-                case 3:
-                    return "An";
-                case 4:
-                    return "Semestru";
-            }
+        if (!isStudent()) {
+            return getDisciplineColumnNames()[columnIndex];
         } else {
-            switch (columnIndex) {
-                case 0:
-                    return "Id";
-                case 1:
-                    return "Nume";
-                case 2:
-                    return "Prenume";
-                case 3:
-                    return "Sex";
-                case 4:
-                    return "Telefon";
-            }
+            return getStudentColumnNames()[columnIndex];
         }
-
-        return "";
     }
 
     @Override
     public Class<?> getColumnClass(int columnIndex) {
-        if (!isStudent) {
-            switch (columnIndex) {
-                case 0:
-                case 4:
-                    return Integer.class;
-            }
+        if (columnIndex == 0) {
+            return Integer.class;
         } else {
-            switch (columnIndex) {
-                case 0:
-                    return Integer.class;
-            }
+            return String.class;
         }
-        return String.class;
     }
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
         return columnIndex == 5;
     }
 
     @Override
     public Object getValueAt(int rowIndex, int columnIndex) {
-        // TODO Auto-generated method stub
-
-        if (!isStudent) {
-            if(rowIndex >= materii.size() || columnIndex > 5) {
-                return null;
-            }
-            Discipline m = materii.get(rowIndex);
-            switch (columnIndex) {
-                case 0:
-                    return m.getId();
-                case 1:
-                    return m.getName();
-                case 2:
-                    return m.getTeacher();
-                case 3:
-                    return m.getYear();
-                case 4:
-                    return m.getSemester();
-            }
+        if (!isStudent()) {
+            return getDisciplineValue(rowIndex, columnIndex);
         } else {
-            if(rowIndex >= studenti.size() || columnIndex > 5) {
-                return null;
-            }
-            Student s = studenti.get(rowIndex);
-            switch (columnIndex) {
-                case 0:
-                    return s.getId();
-                case 1:
-                    return s.getName();
-                case 2:
-                    return s.getSurname();
-                case 3:
-                    return s.getGenre();
-                case 4:
-                    return s.getPhone();
-            }
+            return getStudentValue(rowIndex, columnIndex);
         }
-        return null;
+    }
+
+    public void setDisciplines(List<Discipline> disciplines) {
+        this.disciplines = disciplines;
+    }
+
+    public void setStudents(List<Student> students) {
+        this.students = students;
+    }
+
+    private boolean isStudent() {
+        return students != null;
+    }
+
+    private int getStudentCount() {
+        if (students == null) {
+            return 0;
+        }
+        return students.size();
+    }
+
+    private int getDisciplineCount() {
+        if (disciplines == null) {
+            return 0;
+        }
+        return disciplines.size();
+    }
+
+    private String[] getStudentColumnNames() {
+        return new String[]{"Id", "Nume", "Prenume", "Sex", "Telefon"};
+    }
+
+    private String[] getDisciplineColumnNames() {
+        return new String[]{"Id", "Denumire", "Profesor", "An", "Semestru"};
+    }
+
+    private Object getStudentValue(int rowIndex, int columnIndex) {
+        if (students != null && rowIndex < students.size() && columnIndex <= 4) {
+            return buildStudent(rowIndex, columnIndex);
+        }
+        return new EditareAdapter();
+    }
+
+
+    private Object getDisciplineValue(int rowIndex, int columnIndex) {
+        if (disciplines != null && rowIndex < disciplines.size() && columnIndex <= 5) {
+            return buildDiscipline(rowIndex, columnIndex);
+        }
+        return new EditareAdapter();
+    }
+
+    private Object buildDiscipline(int rowIndex, int columnIndex) {
+        Discipline discipline = disciplines.get(rowIndex);
+        return establishDisciplineStructure(columnIndex, discipline);
+    }
+
+    private static Object establishDisciplineStructure(int columnIndex, Discipline discipline) {
+        switch (columnIndex) {
+            case 0:
+                return discipline.getId();
+            case 1:
+                return discipline.getName();
+            case 2:
+                return discipline.getTeacher();
+            case 3:
+                return discipline.getYear();
+            case 4:
+                return discipline.getSemester();
+            default:
+                throw new IllegalStateException("Unexpected value: " + columnIndex);
+        }
+    }
+
+    private Object buildStudent(int rowIndex, int columnIndex) {
+        Student student = students.get(rowIndex);
+        return establishStudentStructure(columnIndex, student);
+    }
+
+    private static Object establishStudentStructure(int columnIndex, Student student) {
+        switch (columnIndex) {
+            case 0:
+                return student.getId();
+            case 1:
+                return student.getName();
+            case 2:
+                return student.getSurname();
+            case 3:
+                return student.getGenre();
+            case 4:
+                return student.getPhone();
+            default:
+                throw new IllegalStateException("Unexpected value: " + columnIndex);
+        }
     }
 }
