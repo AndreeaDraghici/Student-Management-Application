5781479a87a4630cc6e2bf350814caa3ce2dc8d2
AndreeaDraghici <draghici108@gmail.com>
2023-11-11 11:14:56 +0200

    style: reformatted CatalogFileConstants.java for better readability

--- src/main/java/com/ace/ucv/utils/CatalogFileConstants.java
+++ src/main/java/com/ace/ucv/utils/CatalogFileConstants.java
@@ -13,33 +13,33 @@ public class CatalogFileConstants {
     /**
      * Catalog Element
      */
-    public static final String TAG_NAME                  = "Catalog";
+    public static final String TAG_NAME                          = "Catalog";
 
 
     /**
      * Student Element and Attributes
      */
-    public static final String STUDENT                   = "Student";
-    public static final String STUDENT_NAME              = "Name";
-    public static final String STUDENT_SURNAME           = "Surname";
-    public static final String STUDENT_ID                = "ID";
-    public static final String STUDENT_GENDER            = "Gender";
-    public static final String STUDENT_PHONE            = "Phone";
+    public static final String STUDENT                             = "Student";
+    public static final String STUDENT_NAME                   = "Name";
+    public static final String STUDENT_SURNAME             = "Surname";
+    public static final String STUDENT_ID                       = "ID";
+    public static final String STUDENT_GENDER               = "Gender";
+    public static final String STUDENT_PHONE                 = "Phone";
 
 
     /**
      * Discipline Element and Attributes
      */
-    public static final String DISCIPLINE                = "Discipline";
-    public static final String DISCIPLINE_NAME           = "Name";
-    public static final String DISCIPLINE_ID             = "ID";
-    public static final String DISCIPLINE_TEACHER        = "Teacher";
-    public static final String DISCIPLINE_YEAR           = "Year";
-    public static final String DISCIPLINE_SEMESTER       = "Semester";
+    public static final String DISCIPLINE                         = "Discipline";
+    public static final String DISCIPLINE_NAME               = "Name";
+    public static final String DISCIPLINE_ID                    = "ID";
+    public static final String DISCIPLINE_TEACHER          = "Teacher";
+    public static final String DISCIPLINE_YEAR                = "Year";
+    public static final String DISCIPLINE_SEMESTER        = "Semester";
 
 
     /**
      *   Grade Element
      */
-    public static final String GRADE                     = "Grade";
+    public static final String GRADE                               = "Grade";
 }
