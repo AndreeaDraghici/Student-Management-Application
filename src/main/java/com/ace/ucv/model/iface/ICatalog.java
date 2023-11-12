c375772922038643c94ae4b05508b0538b5b984f
AndreeaDraghici <draghici108@gmail.com>
2023-11-12 18:25:33 +0200

    docs: add detailed comments to ICatalog interface methods

--- src/main/java/com/ace/ucv/model/iface/ICatalog.java
+++ src/main/java/com/ace/ucv/model/iface/ICatalog.java
@@ -8,8 +8,30 @@ import com.ace.ucv.model.Student;
  * Created by Andreea Draghici on 11/12/2023
  * Name of project: StudentManagement
  */
+
+/*
+ * Interface representing a catalog that can store information about students, disciplines, and grades.
+ */
 public interface ICatalog {
+    /**
+     * Adds a student to the catalog.
+     *
+     * @param student The student to be added.
+     */
     void addStudent(Student student);
+
+
+    /**
+     * Adds a discipline to the catalog.
+     *
+     * @param discipline The discipline to be added.
+     */
     void addDiscipline(Discipline discipline);
+
+    /**
+     * Adds a grade to the catalog.
+     *
+     * @param grade The grade to be added.
+     */
     void addGrade(Grade grade);
 }
